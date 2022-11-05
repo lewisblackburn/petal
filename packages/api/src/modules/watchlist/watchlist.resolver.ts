aab600a6fe7821481849361fdc8aa64551503298
lewisblackburn <lewisblackburn10@gmail.com>
2022-11-05 23:46:20 +0000

    fix(watchlist): only request your own watchlist

--- packages/api/src/modules/watchlist/watchlist.resolver.ts
+++ packages/api/src/modules/watchlist/watchlist.resolver.ts
@@ -3,6 +3,10 @@ import { Watchlist, WatchlistWhereUniqueInput } from "@generated"
 import { Inject, Service } from "typedi"
 import { WatchlistService } from "./watchlist.service"
 import { WatchlistInput } from "./inputs/create.input"
+import { CurrentUser } from "../shared/currentUser"
+
+import { User } from "../user/user.model"
+import { UseAuth } from "../shared/middleware/UseAuth"
 
 @Service()
 @Resolver(() => Watchlist)
@@ -10,11 +14,13 @@ export default class WatchlistResolver {
   @Inject(() => WatchlistService)
   watchlistService: WatchlistService
 
+  @UseAuth()
   @Query(() => Watchlist)
-  async watchlist(@Arg("id") id: string) {
-    return this.watchlistService.get(id)
+  async watchlist(@CurrentUser() user: User) {
+    return this.watchlistService.get(user)
   }
 
+  @UseAuth()
   @Mutation(() => Watchlist)
   async updateWatchlist(@Arg("data") data: WatchlistInput, @Arg("where") where: WatchlistWhereUniqueInput) {
     return await this.watchlistService.update(data, where)
--- packages/api/src/modules/watchlist/watchlist.service.ts
+++ packages/api/src/modules/watchlist/watchlist.service.ts
@@ -3,12 +3,13 @@ import { Service } from "typedi"
 import { Resolver } from "type-graphql"
 import { Watchlist, WatchlistWhereUniqueInput } from "@generated"
 import { WatchlistInput } from "./inputs/create.input"
+import { User } from "../user/user.model"
 
 @Service()
 @Resolver(() => Watchlist)
 export class WatchlistService {
-  async get(id: string) {
-    return await prisma.watchlist.findUnique({ where: { id } })
+  async get(user: User) {
+    return await prisma.watchlist.findUnique({ where: { userId: user.id } })
   }
 
   async update(data: WatchlistInput, where: WatchlistWhereUniqueInput) {
