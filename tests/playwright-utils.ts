8d5a3f56d53e9f85b9bd01e4eb9cadc9c82d77f4
lewisblackburn <lewisblackburn10@gmail.com>
2023-07-27 00:09:36 +0100

    send notice email to old email after change

--- tests/playwright-utils.ts
+++ tests/playwright-utils.ts
@@ -89,14 +89,14 @@ export async function loginPage({
 }) {
 	const user = givenUser
 		? await prisma.user.findUniqueOrThrow({
-			where: { id: givenUser.id },
-			select: {
-				id: true,
-				email: true,
-				username: true,
-				name: true,
-			},
-		})
+				where: { id: givenUser.id },
+				select: {
+					id: true,
+					email: true,
+					username: true,
+					name: true,
+				},
+		  })
 		: await insertNewUser()
 	const session = await prisma.session.create({
 		data: {
@@ -121,6 +121,34 @@ export async function loginPage({
 		},
 	])
 	return user
+}
+
+/**
+ * This allows you to wait for something (like an email to be available).
+ *
+ * It calls the callback every 50ms until it returns a value (and does not throw
+ * an error). After the timeout, it will throw the last error that was thrown or
+ * throw the error message provided as a fallback
+ */
+export async function waitFor<ReturnValue>(
+	cb: () => ReturnValue | Promise<ReturnValue>,
+	{
+		errorMessage,
+		timeout = 5000,
+	}: { errorMessage?: string; timeout?: number } = {},
+) {
+	const endTime = Date.now() + timeout
+	let lastError: unknown = new Error(errorMessage)
+	while (Date.now() < endTime) {
+		try {
+			const response = await cb()
+			if (response) return response
+		} catch (e: unknown) {
+			lastError = e
+		}
+		await new Promise(r => setTimeout(r, 100))
+	}
+	throw lastError
 }
 
 test.afterEach(async () => {
