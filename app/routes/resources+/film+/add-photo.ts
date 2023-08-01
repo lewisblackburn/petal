448ba3786a24945e4be51ab2e36b7be59fae6bcc
lewisblackburn <lewisblackburn10@gmail.com>
2023-08-01 16:06:01 +0100

    file upload test

--- app/routes/resources+/film+/add-photo.ts
+++ app/routes/resources+/film+/add-photo.ts
@@ -2,18 +2,18 @@ import { parse } from '@conform-to/zod'
 import {
 	json,
 	type DataFunctionArgs,
-	// unstable_parseMultipartFormData,
-	// type UploadHandler,
+	unstable_parseMultipartFormData,
+	type UploadHandler,
 } from '@remix-run/server-runtime'
-// import { composeUploadHandlers } from '@remix-run/server-runtime/dist/formData.js'
-// import { createMemoryUploadHandler } from '@remix-run/server-runtime/dist/upload/memoryUploadHandler.js'
+import { composeUploadHandlers } from '@remix-run/server-runtime/dist/formData.js'
+import { createMemoryUploadHandler } from '@remix-run/server-runtime/dist/upload/memoryUploadHandler.js'
 import { z } from 'zod'
 import { requireUserId } from '~/utils/auth.server.ts'
 import { MAX_SIZE } from '~/utils/constants.ts'
 import { prisma } from '~/utils/db.server.ts'
 import { redirectWithToast } from '~/utils/flash-session.server.ts'
 import { ensurePE } from '~/utils/misc.tsx'
-// import { s3UploadHandler } from '~/utils/s3.server.ts'
+import { s3UploadHandler } from '~/utils/s3.server.ts'
 import { checkboxSchema } from '~/utils/zod-extensions.ts'
 
 // TODO: Change primary variable to order and the primary photo/video will be the first one
@@ -23,14 +23,14 @@ export const AddFilmPhotoSchema = z.object({
 		typeof window === 'undefined'
 			? z.any()
 			: z
-				.instanceof(File)
-				.refine(
-					file => file.name !== '' && file.size !== 0,
-					'Image is required',
-				)
-				.refine(file => {
-					return file.size <= MAX_SIZE
-				}, 'Image size must be less than 3MB'),
+					.instanceof(File)
+					.refine(
+						file => file.name !== '' && file.size !== 0,
+						'Image is required',
+					)
+					.refine(file => {
+						return file.size <= MAX_SIZE
+					}, 'Image size must be less than 3MB'),
 	type: z.string().nonempty({ message: 'You must select a type' }),
 	language: z.string().nonempty({ message: 'You must select a language' }),
 	primary: checkboxSchema(),
@@ -38,17 +38,16 @@ export const AddFilmPhotoSchema = z.object({
 
 export async function action({ request }: DataFunctionArgs) {
 	await requireUserId(request)
-	// const uploadHandler: UploadHandler = composeUploadHandlers(
-	// 	params =>
-	// 		s3UploadHandler({
-	// 			...params,
-	// 			// TODO: Add randomuuid
-	// 			filename: `films/${params.filename}`,
-	// 		}),
-	// 	createMemoryUploadHandler({ maxPartSize: MAX_SIZE }),
-	// )
-	// const formData = await unstable_parseMultipartFormData(request, uploadHandler)
-	const formData = await request.formData()
+	const uploadHandler: UploadHandler = composeUploadHandlers(
+		params =>
+			s3UploadHandler({
+				...params,
+				// TODO: Add randomuuid
+				filename: `films/${params.filename}`,
+			}),
+		createMemoryUploadHandler({ maxPartSize: MAX_SIZE }),
+	)
+	const formData = await unstable_parseMultipartFormData(request, uploadHandler)
 	const submission = parse(formData, {
 		schema: AddFilmPhotoSchema,
 		acceptMultipleErrors: () => true,
