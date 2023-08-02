22309fae3952399790f903a7a25138981d3ff200
lewisblackburn <lewisblackburn10@gmail.com>
2023-08-02 17:46:55 +0100

    credits table drag handle

--- app/components/table/credits/data-table.tsx
+++ app/components/table/credits/data-table.tsx
@@ -12,7 +12,7 @@ import {
 	useReactTable,
 	getSortedRowModel,
 } from '@tanstack/react-table'
-import React from 'react'
+import React, { useState } from 'react'
 import {
 	Table,
 	TableBody,
@@ -23,6 +23,57 @@ import {
 } from '~/components/ui/table.tsx'
 import { DataTablePagination } from '~/components/table/data-table-pagination.tsx'
 import { DataTableToolbar } from './data-table-toolbar.js'
+import { DndContext, closestCenter } from '@dnd-kit/core'
+import {
+	SortableContext,
+	useSortable,
+	verticalListSortingStrategy,
+} from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+import {
+	restrictToParentElement,
+	restrictToVerticalAxis,
+	restrictToWindowEdges,
+} from '@dnd-kit/modifiers'
+import { Icon } from '~/components/ui/icon.tsx'
+
+const SortableRow = ({ row, data }: { row: any; data: any }) => {
+	const {
+		attributes,
+		listeners,
+		setNodeRef,
+		transform,
+		transition,
+		setActivatorNodeRef,
+	} = useSortable({
+		id: String(data),
+		strategy: verticalListSortingStrategy,
+	})
+	const style = {
+		transition,
+		transform: transform ? CSS.Translate.toString(transform) : undefined,
+		cursor: 'inherit',
+	}
+
+	return (
+		<TableRow
+			key={row.id}
+			ref={setNodeRef}
+			style={style}
+			{...attributes}
+			data-state={row.getIsSelected() && 'selected'}
+		>
+			{row.getVisibleCells().map((cell: any) => (
+				<TableCell key={cell.id}>
+					{flexRender(cell.column.columnDef.cell, cell.getContext())}
+				</TableCell>
+			))}
+			<TableCell ref={setActivatorNodeRef}>
+				<Icon name="drag-handle-dots-2" cursor="grab" {...listeners} />
+			</TableCell>
+		</TableRow>
+	)
+}
 
 interface DataTableProps<TData, TValue> {
 	columns: ColumnDef<TData, TValue>[]
@@ -33,6 +84,7 @@ export function CreditTable<TData, TValue>({
 	columns,
 	data,
 }: DataTableProps<TData, TValue>) {
+	const [dataAsArrayOfIds] = useState(data.map((item: any) => item.id))
 	const [rowSelection, setRowSelection] = React.useState({})
 	const [columnVisibility, setColumnVisibility] =
 		React.useState<VisibilityState>({})
@@ -66,60 +118,80 @@ export function CreditTable<TData, TValue>({
 		getFacetedUniqueValues: getFacetedUniqueValues(),
 	})
 
+	const onDragEnd = (event: any) => {
+		const { active, over } = event
+		if (active.id === over.id) return
+
+		const newIndex = data.findIndex((item: any) => item.id === over.id)
+
+		// set active.id to new index with prisma
+		console.log(active.id, newIndex)
+	}
+
 	return (
-		<div className="space-y-4">
-			<DataTableToolbar table={table} />
-			<div className="rounded-md border">
-				<Table>
-					<TableHeader>
-						{table.getHeaderGroups().map(headerGroup => (
-							<TableRow key={headerGroup.id}>
-								{headerGroup.headers.map(header => {
-									return (
-										<TableHead key={header.id}>
-											{header.isPlaceholder
-												? null
-												: flexRender(
+		<DndContext
+			collisionDetection={closestCenter}
+			onDragEnd={onDragEnd}
+			modifiers={[
+				restrictToVerticalAxis,
+				restrictToWindowEdges,
+				restrictToParentElement,
+			]}
+		>
+			<div className="space-y-4">
+				<DataTableToolbar table={table} />
+				<div className="rounded-md border">
+					<Table>
+						<TableHeader>
+							{table.getHeaderGroups().map(headerGroup => (
+								<TableRow key={headerGroup.id}>
+									{headerGroup.headers.map(header => {
+										return (
+											<TableHead key={header.id}>
+												{header.isPlaceholder
+													? null
+													: flexRender(
 														header.column.columnDef.header,
 														header.getContext(),
-												  )}
-										</TableHead>
-									)
-								})}
-							</TableRow>
-						))}
-					</TableHeader>
-					<TableBody>
-						{table.getRowModel().rows?.length ? (
-							table.getRowModel().rows.map(row => (
-								<TableRow
-									key={row.id}
-									data-state={row.getIsSelected() && 'selected'}
-								>
-									{row.getVisibleCells().map(cell => (
-										<TableCell key={cell.id}>
-											{flexRender(
-												cell.column.columnDef.cell,
-												cell.getContext(),
-											)}
-										</TableCell>
-									))}
+													)}
+											</TableHead>
+										)
+									})}
 								</TableRow>
-							))
-						) : (
-							<TableRow>
-								<TableCell
-									colSpan={columns.length}
-									className="h-24 text-center"
-								>
-									No results.
-								</TableCell>
-							</TableRow>
-						)}
-					</TableBody>
-				</Table>
+							))}
+						</TableHeader>
+						<TableBody>
+							<SortableContext
+								items={dataAsArrayOfIds}
+								strategy={verticalListSortingStrategy}
+							>
+								{table.getRowModel().rows?.length ? (
+									table
+										.getRowModel()
+										.rows.map((row, index) => (
+											<SortableRow
+												key={row.id}
+												row={row}
+												data={dataAsArrayOfIds[index]}
+											/>
+										))
+								) : (
+									<TableRow>
+										<TableCell
+											colSpan={columns.length}
+											className="h-24 text-center"
+										>
+											No results.
+										</TableCell>
+									</TableRow>
+								)}
+							</SortableContext>
+						</TableBody>
+					</Table>
+				</div>
+
+				<DataTablePagination table={table} />
 			</div>
-			<DataTablePagination table={table} />
-		</div>
+		</DndContext>
 	)
 }
