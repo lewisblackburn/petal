cc557a58d3edccbe1de6904e0d352d2a24051d3a
lewisblackburn <lewisblackburn10@gmail.com>
2023-09-13 17:34:59 +0100

    fixed back button

--- app/routes/films+/$filmId.tsx
+++ app/routes/films+/$filmId.tsx
@@ -1,17 +1,22 @@
-import { Outlet, useNavigate } from '@remix-run/react'
+import { Outlet, useLocation, useNavigate } from '@remix-run/react'
 import { Icon } from '#app/components/ui/icon.tsx'
 
 export default function FilmLayout() {
+	const location = useLocation()
 	const navigate = useNavigate()
+
+	const handleClick = () => {
+		const parts = location.pathname.split('/')
+		parts.pop()
+		const newPath = parts.join('/')
+		navigate(newPath)
+	}
 
 	return (
 		<div>
 			<div className="flex h-16 items-center border-b">
 				<div className="container py-6">
-					<button
-						className="flex items-center gap-2"
-						onClick={() => navigate(-1)}
-					>
+					<button className="flex items-center gap-2" onClick={handleClick}>
 						<Icon name="arrow-left" />
 						<span>Back</span>
 					</button>
