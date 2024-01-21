f3810dac0f5b5d63da458d4adadc16b80482f79b
lewisblackburn <lewisblackburn10@gmail.com>
2024-01-21 23:48:36 +0000

    added permissions and roles to prisma migration sql script

--- prisma/migrations/20240121161704_initial/migration.sql
+++ prisma/migrations/20240121161704_initial/migration.sql
@@ -71,6 +71,43 @@ CREATE TABLE "Permission" (
     "updatedAt" DATETIME NOT NULL
 );
 
+-- Create Permissions
+INSERT INTO Permission VALUES('clrnsxdsc0000gnf4og0vzt1a','create','user','own','',1705859877900,1705859877900);
+INSERT INTO Permission VALUES('clrnsxdsh0001gnf463ashegk','create','user','any','',1705859877905,1705859877905);
+INSERT INTO Permission VALUES('clrnsxdsm0002gnf4r8ugw2q5','read','user','own','',1705859877910,1705859877910);
+INSERT INTO Permission VALUES('clrnsxdsq0003gnf4vhbzlk9j','read','user','any','',1705859877915,1705859877915);
+INSERT INTO Permission VALUES('clrnsxdsv0004gnf4g5mxo2xn','update','user','own','',1705859877920,1705859877920);
+INSERT INTO Permission VALUES('clrnsxdt00005gnf4yuobac43','update','user','any','',1705859877924,1705859877924);
+INSERT INTO Permission VALUES('clrnsxdt50006gnf4wij7a4i7','delete','user','own','',1705859877929,1705859877929);
+INSERT INTO Permission VALUES('clrnsxdt90007gnf4b3ynjvnr','delete','user','any','',1705859877934,1705859877934);
+
+INSERT INTO Permission VALUES('clrnsxdte0008gnf4ubh5q7vb','create','note','own','',1705859877939,1705859877939);
+INSERT INTO Permission VALUES('clrnsxdtj0009gnf46uxuqtni','create','note','any','',1705859877943,1705859877943);
+INSERT INTO Permission VALUES('clrnsxdto000agnf4hh8y7u0p','read','note','own','',1705859877948,1705859877948);
+INSERT INTO Permission VALUES('clrnsxdts000bgnf4pifq8pgd','read','note','any','',1705859877953,1705859877953);
+INSERT INTO Permission VALUES('clrnsxdtx000cgnf4m48axmqw','update','note','own','',1705859877958,1705859877958);
+INSERT INTO Permission VALUES('clrnsxdu2000dgnf4ztyjlu88','update','note','any','',1705859877962,1705859877962);
+INSERT INTO Permission VALUES('clrnsxdu7000egnf4jtf1s1ab','delete','note','own','',1705859877967,1705859877967);
+INSERT INTO Permission VALUES('clrnsxdub000fgnf4hwjjv1ou','delete','note','any','',1705859877972,1705859877972);
+
+INSERT INTO Permission VALUES('clrnsxdug000ggnf4at7zq5j9','create','film','own','',1705859877977,1705859877977);
+INSERT INTO Permission VALUES('clrnsxdul000hgnf4gzhoh15u','create','film','any','',1705859877981,1705859877981);
+INSERT INTO Permission VALUES('clrnsxduq000ignf4cd1fnqq2','read','film','own','',1705859877986,1705859877986);
+INSERT INTO Permission VALUES('clrnsxduu000jgnf4w2mi4k6d','read','film','any','',1705859877991,1705859877991);
+INSERT INTO Permission VALUES('clrnsxduz000kgnf45t1xtn8a','update','film','own','',1705859877995,1705859877995);
+INSERT INTO Permission VALUES('clrnsxdv4000lgnf48vnc18sq','update','film','any','',1705859878000,1705859878000);
+INSERT INTO Permission VALUES('clrnsxdv8000mgnf4j136uo5v','delete','film','own','',1705859878005,1705859878005);
+INSERT INTO Permission VALUES('clrnsxdvh000ngnf42p7ja7po','delete','film','any','',1705859878013,1705859878013);
+
+INSERT INTO Permission VALUES('clrnsxdvl000ognf4ff7tpthv','create','review','own','',1705859878018,1705859878018);
+INSERT INTO Permission VALUES('clrnsxdvq000pgnf4mxgm0l12','create','review','any','',1705859878023,1705859878023);
+INSERT INTO Permission VALUES('clrnsxdvv000qgnf4ff5jvkza','read','review','own','',1705859878027,1705859878027);
+INSERT INTO Permission VALUES('clrnsxdw0000rgnf4gat9d9zd','read','review','any','',1705859878032,1705859878032);
+INSERT INTO Permission VALUES('clrnsxdw4000sgnf4mjtcn4j8','update','review','own','',1705859878037,1705859878037);
+INSERT INTO Permission VALUES('clrnsxdw9000tgnf4en0fzv56','update','review','any','',1705859878042,1705859878042);
+INSERT INTO Permission VALUES('clrnsxdwe000ugnf4z5z3kahx','delete','review','own','',1705859878046,1705859878046);
+INSERT INTO Permission VALUES('clrnsxdwj000vgnf40cixwl3u','delete','review','any','',1705859878051,1705859878051);
+
 -- CreateTable
 CREATE TABLE "Role" (
     "id" TEXT NOT NULL PRIMARY KEY,
@@ -79,6 +116,10 @@ CREATE TABLE "Role" (
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
+
+-- Create Roles
+INSERT INTO Role VALUES('clrnsxdwo000wgnf44pcdcwa5','admin','',1705859878057,1705859878057);
+INSERT INTO Role VALUES('clrnsxdwu000xgnf43y4nmolr','user','',1705859878062,1705859878062);
 
 -- CreateTable
 CREATE TABLE "Verification" (
