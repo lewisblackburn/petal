147f67d5198ad8a107ae03402b5454c2b37ac58f
lewisblackburn <lewisblackburn10@gmail.com>
2024-01-22 17:35:40 +0000

    dashboard initial setup

--- app/components/layout/index.tsx
+++ app/components/layout/index.tsx
@@ -1,26 +1,47 @@
 import { invariant } from '@epic-web/invariant'
-import { Link, Outlet, useRouteLoaderData } from '@remix-run/react'
+import { Link, Outlet, useMatches, useRouteLoaderData } from '@remix-run/react'
 import { ThemeSwitch, type loader as rootLoader } from '#app/root'
-import { NavigationBar } from '../navigation-bar'
+import { NavigationBar } from './navigation-bar'
 
 export const Layout = () => {
 	const data = useRouteLoaderData<typeof rootLoader>('root')
+	const matches = useMatches()
 	invariant(data?.requestInfo, 'No requestInfo found in root loader')
 
-	return (
-		<div className="flex h-screen flex-col justify-between">
-			<NavigationBar />
-
-			<div className="flex-1">
-				<Outlet />
-			</div>
-
-			<div className="container flex justify-between py-10">
-				<Logo />
-				<ThemeSwitch userPreference={data.requestInfo.userPrefs.theme} />
-			</div>
-		</div>
+	const isOnDashboardPage = !!matches.find(
+		m => m.pathname === '/admin/dashboard',
 	)
+
+	if (isOnDashboardPage) {
+		return (
+			<div className="flex h-screen flex-col justify-between">
+				<NavigationBar />
+
+				<div className="flex-1">
+					<Outlet />
+				</div>
+
+				<div className="container flex justify-between py-10">
+					<Logo />
+					<ThemeSwitch userPreference={data.requestInfo.userPrefs.theme} />
+				</div>
+			</div>
+		)
+	} else
+		return (
+			<div className="flex h-screen flex-col justify-between">
+				<NavigationBar />
+
+				<div className="flex-1">
+					<Outlet />
+				</div>
+
+				<div className="container flex justify-between py-10">
+					<Logo />
+					<ThemeSwitch userPreference={data.requestInfo.userPrefs.theme} />
+				</div>
+			</div>
+		)
 }
 
 function Logo() {
