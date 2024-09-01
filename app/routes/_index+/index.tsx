23f4e3d544934681b9c11245f5fa0e953af32011
Lewis Blackburn <lewisblackburn10@gmail.com>
2024-09-01 16:02:01 +0100

    chore: Add framer-motion package for animations

--- app/routes/_index+/index.tsx
+++ app/routes/_index+/index.tsx
@@ -15,7 +15,7 @@ export default function Index() {
 function Hero() {
 	return (
 		<main className="bg-secondary">
-			<div className="mx-auto max-w-7xl px-4 py-16 sm:py-24">
+			<div className="mx-auto max-w-7xl px-4 py-24 sm:py-32">
 				<div className="text-center">
 					<h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
 						<span className="block xl:inline">Linking Worlds</span>{' '}
@@ -46,46 +46,54 @@ function Hero() {
 	)
 }
 
+const Logos = [
+	// film, song, game, book, people website related lgoo
+	{
+		name: 'TMDB',
+		url: 'https://www.themoviedb.org/',
+		image: '/images/logos/tmdb.png',
+	},
+	{
+		name: 'Spotify',
+		url: 'https://www.spotify.com/',
+		image: '/images/logos/spotify.png',
+	},
+	{
+		name: 'Steam',
+		url: 'https://store.steampowered.com/',
+		image: '/images/logos/steam.png',
+	},
+	{
+		name: 'Goodreads',
+		url: 'https://www.goodreads.com/',
+		image: '/images/logos/goodreads.png',
+	},
+	{
+		name: 'IMDb',
+		url: 'https://www.imdb.com/',
+		image: '/images/logos/imdb.png',
+	},
+]
+
 function LogoCloud() {
 	return (
 		<div className="bg-secondary">
-			<div className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
+			<div className="mx-auto max-w-7xl px-4 pb-32 pt-12 sm:px-6 lg:px-8">
 				<div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
-					<div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-						<img
-							className="h-12"
-							src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
-							alt="Tuple"
-						/>
-					</div>
-					<div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-						<img
-							className="h-12"
-							src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg"
-							alt="Mirage"
-						/>
-					</div>
-					<div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-						<img
-							className="h-12"
-							src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
-							alt="StaticKit"
-						/>
-					</div>
-					<div className="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
-						<img
-							className="h-12"
-							src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
-							alt="Transistor"
-						/>
-					</div>
-					<div className="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
-						<img
-							className="h-12"
-							src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg"
-							alt="Workcation"
-						/>
-					</div>
+					{Logos.map((logo) => (
+						<div
+							key={logo.name}
+							className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1"
+						>
+							<a href={logo.url}>
+								<img
+									className="h-10 grayscale"
+									src={logo.image}
+									alt={logo.name}
+								/>
+							</a>
+						</div>
+					))}
 				</div>
 			</div>
 		</div>
