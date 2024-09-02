fb4bece0f3ed1a4e8979861d2dfca3fa50aa2243
Lewis Blackburn <lewisblackburn10@gmail.com>
2024-09-02 18:47:15 +0100

    chore: Update sidebar width for dashboard layout

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -67,7 +67,7 @@ export default function DashboardPageLayout() {
 	return (
 		<div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
 			<div className="hidden border-r bg-muted/40 md:block">
-				<div className="flex h-full max-h-screen flex-col gap-2">
+				<div className="fixed flex h-full max-h-screen flex-col gap-2 md:w-[220px] lg:w-[280px]">
 					<div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
 						<Logo />
 						<Button variant="outline" size="icon" className="ml-auto h-8 w-8">
